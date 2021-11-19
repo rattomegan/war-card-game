3eebb3a2d798234954200c61984bec623ecd1118
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2021-11-19 13:55:33 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// create contestants objects with multiple arrays within: main pile for pulling, pile for card gains, pile for cards on board
+//  player deck and player hand (current card)
+
+// create master deck array. 
+// asign image values to each index number in master array
+
+// event listeners
+// 1. start game
+// 2. play card
+
+//initialize the game:
+//  1. split values from the master deck array randomly to each player array
+
+// handle turn:
+// 1. function to pull first value index from player 1 array
+//      a. display card image based on array
+// 2. function to pull first value card from computer array
+//      a. display card image based on array index
+// 3. compare value of player1 card to computer.
+//      a. if player1 card is higher than computer, add card to bottom of player 1 array
+//      b. if computer card is higher, add card to end of computer array
+//      c. if value is the same - run tie scenario - link to tie function? or keep inside?
+//          i. tie function:
+//              - pull three more idx face down, fourth one is revealed.
+//              - compare fourth card, run tie function again if needed.
+
+// win scenario and variable
+// 1. when one player array length equals 52 or one array is empty declare winner
+
+// handle player wanting to replay
