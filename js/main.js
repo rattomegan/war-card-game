b337e421eca5cb0820dfba23bfd006c38469901e
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2021-11-22 16:30:10 -0800

    not sure what changes I've made here -just saving at the end of Monday 11.22

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+/*----- constants -----*/
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
 // this is needed to update the ranking value in our build master deck function
 const cardLookUp = {
     'J': 11,
@@ -8,99 +10,229 @@ const cardLookUp = {
     'A': 14,
 };
 
+// Build a 'master' deck of 'card' objects used to create shuffled decks
 // this is now an array of objects with face and value as keys.
 const masterDeck = buildMasterDeck();
 
-function buildMasterDeck() {
-    const deck = [];
-    // Use nested forEach to generate card objects
-    suits.forEach(function(suit) {
-      ranks.forEach(function(rank) {
-        deck.push({
-          // The 'face' property maps to the library's CSS classes for cards
-          face: `${suit}${rank}`,
-          // this is setting the value of the rank from either the rank array or our cardLookUp object for the face cards.
-          value: Number(rank) || cardLookUp[rank]
-        });
-      });
-    });
-    return deck;
-  }
-
-const player1 = {
-    // holds deck of cards to be played and cards that are won will be pushed to the end of the array
-    pile: [],
-    // holds the current card played
-    playCard: [],
-    // the four war cards in a war scenario
-    warCards: [],
-}
-
-const computer = {
-    pile: [],
-    playCard: [],
-    warCards: [],
-}
-
-// -----------------------------------------------------------
-// don't touch anything above this line.
 
 /*----- app's state (variables) -----*/
-let gameDeck, winner;
-let newShuffledDeck;
+// // this variable is updated in the original renderNewShuffledDeck function.
+// let shuffledDeck;
+
+
+// WonPiles will need to be shuffled at some point.
+let pPile, pHand;
+let cPile, cHand;
+let scores, cardRank, winner; // will eventually be p or c
+
+
+
+// I've moved this out of the getNewShuffledDeck function and made it a let variable so it can change - this doesn't actually matter. it works as a const variable as well.
+const newShuffledDeck = [];
+
 
 /*----- cached element references -----*/
 
-const warEls = {
-  pCard1: '',
-  pCard2: '',
-  pCard3: '',
-  pCard4: '',
-  cCard1: '',
-  cCard2: '',
-  cCard3: '',
-  cCard4: '',
+const cardEls = {
+    p: {
+      pile: document.getElementById('p-pile'),
+      0: document.getElementById('p-card0'),
+      1: document.getElementById('p-card1'),
+      2: document.getElementById('p-card2'),
+      3: document.getElementById('p-card3'),
+    },
+    c: {
+      pile: document.getElementById('c-pile'),
+      0: document.getElementById('c-card0'),
+      1: document.getElementById('c-card1'),
+      2: document.getElementById('c-card2'),
+      3: document.getElementById('c-card3'),
+    }
+  }
+
+const scoreEls = {
+  p: document.getElementById('p-score'),
+  c: document.getElementById('c-score'),
 }
 
-const pCardEl = document.getElementById('p-card1');
-const pBackEl = document.getElementById('p-pile');
-const cCardEl = document.getElementById('c-card1');
-const cBackEl = document.getElementById('c-pile');
-
-/*----- event listeners -----*/
-// document.getElementById('start-game').addEventListener('click', renderGame);
-// // this button will need a function that changes the play card button to "war" or something similar
-// document.getElementById('play-card').addEventListener('click', playRound);
-// document.getElementById('replay').addEventListener('click', renderGame);
+  // button will change text depending on point in game
+const buttonEl = document.getElementById('button');
 
 /*----- functions -----*/
-
-// init();
+function getNewShuffledDeck() {
+    // Create a copy of the masterDeck (leave masterDeck untouched!)
+    const tempDeck = [...masterDeck];
+    // commenting below line out temporarily as I've moved it outside of this function.
+    // const newShuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
+  }
+  
+function buildMasterDeck() {
+  const deck = [];
+  // Use nested forEach to generate card objects
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // The 'face' property maps to the library's CSS classes for cards
+        face: `${suit}${rank}`,
+          // this is setting the value of the rank from either the rank array or our cardLookUp object for the face cards.
+        value: Number(rank) || cardLookUp[rank]
+      });
+    });
+  });
+  return deck;
+}
 
 function init() {
   buildMasterDeck();
-  getNewShuffledDeck();
-  // update game deck by splitting array with splice
-  function dealDeck() {
-    player1.pile = newShuffledDeck.splice(0, (newShuffledDeck.length / 2))
-    console.log(player1.pile)
-    computer.pile = newShuffledDeck.splice();
-  };
   dealDeck();
+  scores = {
+    p: pPile.length,
+    c: pPile.length
+  }
+  cardRank = {
+    p: 0,
+    c: 0
+  }
+  cHand = [];
+  pHand = [];
+  winner = null;
+
+  // need to add render game board here. still need to write that function below.
 }  
 
-function getNewShuffledDeck() {
-  // Create a copy of the masterDeck (leave masterDeck untouched!)
-  const tempDeck = [...masterDeck];
-  const newShuffledDeck = [];
-  while (tempDeck.length) {
-    // Get a random index for a card still in the tempDeck
-    const rndIdx = Math.floor(Math.random() * tempDeck.length);
-    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-  }
-  return newShuffledDeck;
+init();
+
+
+function dealDeck() {
+  getNewShuffledDeck();  
+  // here we are splitting the newShuffledDeck in 2 and assigning to the game piles
+  pPile = newShuffledDeck.splice(0, (newShuffledDeck.length / 2))
+  cPile = newShuffledDeck.splice(0, newShuffledDeck.length);
+  // newShuffleDeck is now an empty array since we spliced out all the elements.
+};
+  
+
+
+
+function handleTurn(evt) {
+    // when player clicks their card deck or play card button (this will be referenced from an event listener.)
+  // check for win scenario  
+  if (pPile.length === 0 || cPile.length === 0) return getWinner();
+    // pull first value from player pile put it in play card array.
+  // these methods are not working - pausing to continue with game logic
+  pHand.push(pPile.shift());
+  cHand.push(cPile.shift());
+  if (pHand[0].value === cHand[0].value) return runWar();
+  // update these to splice instead of spread push. could also do pHand.length instead of (0, 1) - tried this below and got an infinite loop in running war.
+  console.log(pHand)
+  pHand[0].value > cHand[0].value ? pPile.push(...pHand.splice(0, 1), ...cHand.splice(0, 1)) : cPile.push(...pHand.splice(0, 1), ...cHand.splice(0, 1));
+  // update scores and render score board.
+  console.log(winner);
+  scores.p = pPile.length;
+  scores.c = cPile.length;
+  renderScores();
+  };
+
+function runWar() {
+  // the render function here will need to reference the index number of the war array to match the id of the card <div>
+  // if (pPile.length < 4 || cPile.length < 4) // send to run win scenario? and have a comparison of scores there?
+  console.log("Running War")
+  pHand.push(...pPile.splice(0, 4));
+  cHand.push(...cPile.splice(0, 4));
+  if (pHand[3].value === cHand[3].value) {
+    return runWar();
+  } else if (pHand[3].value > cHand[3].value) {
+      winner = "p";  
+      pPile.push(...pHand.splice(0, 5));
+      pPile.push(...cHand.splice(0, 5));
+  } else {
+      winner = "c";
+      cPile.push(...pHand.splice(0, 5));
+      cPile.push(...cHand.splice(0, 5));
+    }
+    scores.p = pPile.length;
+    scores.c = cPile.length;
+    renderScores();
+// maybe return a render function here? where the cards physically move to the win pile and off the board.
 };
 
-getNewShuffledDeck();
 
+// I will need to revisit this function tomorrow as I'm not sure it will wrok properly.
+function getWinner() {
+  if(pPile.length > cPile.length) {
+    winner = "p";
+  } else {
+    winner = "c";
+  }
+};
+
+function renderScores() {
+  // render the scores
+  for (let score in scores) {
+    scoreEls[score].textContent = scores[score];
+  };
+};
+
+function renderCards(hand, container) {
+  for (let i = 0; i < hand.length; i++) {
+    container.innerHTML = `<div id="c-card${i} class="card ${card.face}"></div>`
+
+  }
+} 
+  // let cardsHTML = '';
+  // hand.forEach(function(card){ 
+  //   cardsHTML += `<div class="card ${card.face} id="c-card${hand[i]"></div>`;
+  // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // // This is my original runWar function that includes warPiles. I have removed them to test just having one play pile.
+// function runWar() {
+//     // the render function here will need to reference the index number of the war array to match the id of the card <div>
+//     // if (pPile.length < 4 || cPile.length < 4) run if statemetns below but for the lenght of the array. .length + 1 to include last array index;
+//     pWarPile = pPile.splice(0, 4);
+//     cWarPile = cPile.splice(0, 4);
+//     // it is okay for this to be push right now since the war pile will be updated when the function reruns and the old values will be updated.
+//     // this is currently pushing the whole array into the other array, creating separate arrays within the winPile. 
+//     // return pWarPile[3].value > cWarPile[3].value ? pWinPile.push(pWarPile, cWarPile) : cWinPile.push(pWarPile, cWarPile)
+//     if (pWarPile[3].value === cWarPile[3].value) {
+//       runWar();
+//     } else if (pWarPile[3].value > cWarPile[3].value) {
+//       winner = "p";  
+//       pWarPile.forEach(function(card) {
+//             pPile.push(card);
+//         })
+//         cWarPile.forEach(function(card){
+//             pPile.push(card);
+//         })
+//     } else {
+//         winner = "c";
+//         pWarPile.forEach(function(card) {
+//             cPile.push(card);
+//         })
+//         cWarPile.forEach(function(card){
+//             cPile.push(card);
+//         })
+//       }
+//     // I'm not sure what to have it return here.
+//     return winner // maybe return a render function here? where the cards physically move to the win pile and off the board.
+// };
+// 
